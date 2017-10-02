208e5803a37898c3ecbae3a4c00f2e99b32f5532
jourdier adrien <adrien.jourdier@eemi.com>
2017-10-02 17:42:30 +0200

    Add error.php file

--- app/model/pdo.inc.php
+++ app/model/pdo.inc.php
@@ -2,9 +2,9 @@
 if (!defined("_BASE_URL")) die("Ressource interdite !");
 try
 {
-	$dns = 'mysql:host=localhost; dbname=';
+	$dns = 'mysql:host=localhost; dbname=reseau_social';
 	$utilisateur = 'root';
-	$mdp = '';
+	$mdp = 'root';
 	$options = array(
 		PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
 		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
